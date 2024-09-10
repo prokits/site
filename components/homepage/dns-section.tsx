8f3e0f20ab32c3f4a9b957d40216d23551f4e215
Tushar Gaurav <tushar.gaurav416@gmail.com>
2024-09-10 08:34:58 +0530

    init

---
+++ components/homepage/dns-section.tsx
@@ -0,0 +1,16 @@
+export default function DNSSection() {
+    return (
+        <div>
+            <h3 className="text-red_branding font-semibold text-4xl">Private DNS</h3>
+            <h1>Fast. Free. Private.</h1>
+            <div>
+                <p>
+                    Your ISP can see every site and app you use, even if they&apos;re encrypted. Some providers even sell this data, or use it to target you with ads.
+                </p>
+                <p>
+                    Prokits DNS prevents anyone from snooping on you by encrypting more of the traffic leaving your device. We believe privacy is a right. We won&apos;t sell your data, ever.
+                </p>
+            </div>
+        </div>
+    )
+}
