d23eeffe3ca21b4622e801ab55fccb7481a357be
Tushar Gaurav <tushar.gaurav416@gmail.com>
2025-02-25 18:18:19 +0530

    feat: add odoo livechat widget

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,26 @@ import DNSSection from "@/components/homepage/dns-section";
 import GlobeExperience from "@/components/homepage/globe/experience";
 import SpeedUploads from "@/components/homepage/speedyuploads";
 import CurvedSeperator from "@/components/ui/curved-seperator";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div>
+      {/* Odoo Live Chat */}
+      <Script  defer type="text/javascript" src="https://odoo.prokits.digital/im_livechat/loader/1"></Script> 
+      <Script defer type="text/javascript" src="https://odoo.prokits.digital/im_livechat/assets_embed.js"></Script>
+
       <section className="bg-black">
         <GlobeExperience />
       </section>
 
       <section className="bg-zinc-950">
-        <svg width="w-screen" height="80" viewBox="0 0 500 80" preserveAspectRatio="none">
+        <svg
+          width="w-screen"
+          height="80"
+          viewBox="0 0 500 80"
+          preserveAspectRatio="none"
+        >
           <path d="M0,0 L0,40 Q250,100 500,40 L500,0 Z" fill="black" />
         </svg>
         <DNSSection />
@@ -22,7 +32,6 @@ export default function Home() {
         <CurvedSeperator flipped={true}>
           <SpeedUploads />
         </CurvedSeperator>
-
       </section>
     </div>
   );
